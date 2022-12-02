7817a6d5b46634cfe1fd672f60bfce00c8124d3c
Jeron <norejudeisa@gmail.com>
2022-12-02 19:15:45 +0000

    refactor valid group validation

--- lib/shared/validation/validator.dart
+++ lib/shared/validation/validator.dart
@@ -36,7 +36,14 @@ class Validator {
       return "Come on, enter the group code";
     } else if (!value.endsWith("grp") && !value.endsWith("subgrp")) {
       return "Invalid group code";
+    } else if (value.endsWith('grp') && value.length < 10) {
+      return "Invalid group code";
+    } else if (value.endsWith('subgrp') && value.length < 13) {
+      return "Invalid group code";
+    } else if (value.length > 13) {
+      return "Invalid group code";
     }
+
     return null;
   }
 }
